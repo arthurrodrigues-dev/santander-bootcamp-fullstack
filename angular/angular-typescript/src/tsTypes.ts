1adbf0f9604f6e1aa0d8ca3d4a46dae6b1b1be7d
arthur <arthurrodrigues.contato@outlook.com>
2023-08-29 21:12:25 -0300

    ts types

---
+++ angular/angular-typescript/src/tsTypes.ts
@@ -0,0 +1,16 @@
+// primitive types: boolean, number, string
+
+let isON: boolean = false
+let Name: string = "Arthur"
+let age: number = 30
+let height: number = 1.8
+
+// special types
+//null
+//undefined
+let nulll: null = null
+let undefinedd: undefined = undefined
+
+// any, void
+let returnn:void
+let returnView:any = false
