dc43a2b31d489c6fe9a8ad03f0285355b59f68b2
arthur <arthurrodrigues.contato@outlook.com>
2023-08-31 11:58:57 -0300

    objects, arrays, tuples ts

---
+++ angular/angular-typescript/build/tsTypes.js
@@ -0,0 +1,26 @@
+"use strict";
+// primitive types: boolean, number, string
+let isON = false;
+let Name = "Arthur";
+let age = 30;
+let height = 1.8;
+// special types
+//null
+//undefined
+let nulll = null;
+let undefinedd = undefined;
+// any, void
+let retorno;
+let returnView = false;
+//object
+let product = {
+    name: "felipe",
+    cidade: "sp",
+    idade: 30
+};
+console.log(product);
+let meuProduto = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 5
+};
--- angular/angular-typescript/src/tsTypes.ts
+++ angular/angular-typescript/src/tsTypes.ts
@@ -12,5 +12,36 @@ let nulll: null = null
 let undefinedd: undefined = undefined
 
 // any, void
-let returnn:void
+let retorno:void
 let returnView:any = false
+
+//object
+
+let product: object = {
+    name: "felipe",
+    cidade: "sp",
+    idade: 30
+}
+
+type ProdutoLoja = {
+    nome: string,
+    preco: number,
+    unidades: number
+}
+
+let meuProduto: ProdutoLoja = {
+    nome: "Tênis",
+    preco: 89.99,
+    unidades: 5
+}
+
+// Arrays
+
+let dados: string[] = ["felipe", "ana", "adriana"]
+let dados2: Array<string> = ["felipe", "ana", "adriana"]
+
+let infos: (string | number)[] = ["felipe", 30, "arthur", "adriana", 22]
+
+// Tuples
+
+let boleto:[string, number, number] = ["agua conta", 199.90, 323232343]
