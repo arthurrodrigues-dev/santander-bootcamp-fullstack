e3e281de7c2d912114def63719fa5073f32281d9
arthur <arthurrodrigues.contato@outlook.com>
2023-08-31 12:14:28 -0300

    ts functins

--- angular/angular-typescript/build/index.js
+++ angular/angular-typescript/build/index.js
@@ -6,3 +6,17 @@ printaObjetos({
     name: "bruce wayne",
     vulgo: "batman"
 });
+// ts functions
+function addNumber(x, y) {
+    return x + y;
+}
+function addToHello(name) {
+    return `Hello ${name}`;
+}
+function CallToPhone(phone) {
+    return phone;
+}
+let soma = addNumber(4, 7);
+console.log(soma);
+console.log(addToHello("Arthur"));
+console.log(CallToPhone("1234"));
--- angular/angular-typescript/src/index.ts
+++ angular/angular-typescript/src/index.ts
@@ -11,3 +11,29 @@ printaObjetos({
     name: "bruce wayne",
     vulgo: "batman"
 })
+
+// ts functions
+
+function addNumber(x: number, y: number): number {
+    return x + y
+}
+
+function addToHello(name: string): string {
+    return `Hello ${name}`
+}
+
+function CallToPhone(phone: number | string): number | string {
+    return phone
+}
+
+async function getDatabase(id: number): Promise<string> {
+    return "felipe"
+}
+
+let soma: number = addNumber(4, 7)
+console.log(soma);
+
+console.log(addToHello("Arthur"));
+
+console.log(CallToPhone("1234"));
+
